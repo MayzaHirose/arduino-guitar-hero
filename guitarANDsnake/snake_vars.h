7efdaf0c8259d7390ebb373a5a92b28bc3fbfcb3
MayzaHirose <maayhirose@gmail.com>
2017-07-29 18:51:16 -0300

    Dois Jogos. PROGMEM adicionado pois a memoria estava cheia

---
+++ guitarANDsnake/snake_vars.h
@@ -0,0 +1,17 @@
+#define ESQ    1 //Direcao esquerda
+#define CIMA   2 //Direcao cima
+#define DIR   -1 //Direcao direita
+#define BAIXO -2 //Direcao Baixo
+#define COMIDA 3 //Flag Comida
+
+//Variáveis Snake
+int matriz[8][8];
+int tamSnake = 1;
+int direcaoAtual = ESQ;
+int proximaDirecao = ESQ;
+int cabecaX = 0;
+int cabecaY = 0;
+int proximoX = 0;
+int proximoY = 0;
+boolean bateu = false;
+boolean comeu = false;
